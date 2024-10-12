a822c15058e45ab9327024da24c9dadd50741210
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2024-10-12 03:46:54 +0200

    Updated module imports.

--- client/src/ts/signUp.ts
+++ client/src/ts/signUp.ts
@@ -3,7 +3,10 @@ import '../scss/main.scss';
 import topNavbar from './modules/global/topNavbar';
 import botNavbar from './modules/global/botNavbar';
 import { signUpForm } from './modules/signUp/signUpForm';
+import { verificationForm } from './modules/signUp/verificationForm';
 
 topNavbar();
 botNavbar();
+
+verificationForm();
 signUpForm();
