ca0bffe8172d9e3e213634a991e6cac8fb27f74d
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2024-10-16 04:06:08 +0200

    Created hangout.html and hangout.ts.

---
+++ client/src/ts/hangout.ts
@@ -0,0 +1,8 @@
+import '../scss/main.scss';
+
+import topNavbar from './modules/global/topNavbar';
+import botNavbar from './modules/global/botNavbar';
+
+// initializing imports
+topNavbar();
+botNavbar();
