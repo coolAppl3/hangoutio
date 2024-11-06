09a5b0c1272771b5810c89e73f1b34944d736478
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2024-11-06 03:17:05 +0100

    Added POST chat/retrieve

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -134,3 +134,124 @@ chatRouter.post('/add', async (req: Request, res: Response) => {
     res.status(500).json({ success: false, message: 'Internal server error.' });
   };
 });
+
+chatRouter.post('/retrieve', async (req: Request, res: Response) => {
+  interface RequestData {
+    hangoutID: string,
+    hangoutMemberID: number,
+    messageOffset: number,
+  };
+
+  const authHeader: string | undefined = req.headers['authorization'];
+  if (!authHeader) {
+    res.status(401).json({ success: false, message: 'Invalid credentials. Request denied.' });
+    return;
+  };
+
+  const authToken: string = authHeader.substring(7);
+  if (!isValidAuthToken(authToken)) {
+    res.status(401).json({ success: false, message: 'Invalid credentials. Request denied.' });
+    return;
+  };
+
+  const userID: number = getUserID(authToken);
+  const requestData: RequestData = req.body;
+
+  const expectedKeys: string[] = ['hangoutID', 'hangoutMemberID', 'messageOffset'];
+  if (undefinedValuesDetected(requestData, expectedKeys)) {
+    res.status(400).json({ success: false, message: 'Invalid request data.' });
+    return;
+  };
+
+  if (!isValidHangoutID(requestData.hangoutID)) {
+    res.status(400).json({ success: false, message: 'Invalid hangout ID.', reason: 'hangoutID' });
+    return;
+  };
+
+  if (!Number.isInteger(requestData.hangoutMemberID)) {
+    res.status(400).json({ success: false, message: 'Invalid hangout member ID', reason: 'hangoutMemberID' });
+    return;
+  };
+
+  if (!Number.isInteger(requestData.messageOffset)) {
+    res.status(400).json({ success: false, message: 'Invalid messages offset.', reason: 'messageOffset' });
+    return;
+  };
+
+  try {
+    interface UserDetails extends RowDataPacket {
+      auth_token: string,
+    };
+
+    const userType: 'account' | 'guest' = getUserType(authToken);
+    const [userRows] = await dbPool.execute<UserDetails[]>(
+      `SELECT
+        auth_token
+      FROM
+        ${userType}s
+      WHERE
+        ${userType}_id = ?;`,
+      [userID]
+    );
+
+    if (userRows.length === 0) {
+      res.status(401).json({ success: false, message: 'Invalid credentials. Request denied.' });
+      return;
+    };
+
+    if (authToken !== userRows[0].auth_token) {
+      res.status(401).json({ success: false, message: 'Invalid credentials. Request denied.' });
+      return;
+    };
+
+    const [hangoutRows] = await dbPool.execute<RowDataPacket[]>(
+      `SELECT
+        1
+      FROM
+        hangout_members
+      WHERE
+        hangout_member_id = ? AND
+        ${userType}_id = ? AND
+        hangout_id = ?;`,
+      [requestData.hangoutMemberID, userID, requestData.hangoutID]
+    );
+
+    if (hangoutRows.length === 0) {
+      res.status(404).json({ success: false, message: 'Hangout not found.' });
+      return;
+    };
+
+    interface ChatMessage extends RowDataPacket {
+      message_id: number,
+      hangout_member_id: number,
+      message_content: string,
+      message_timestamp: number,
+      sender_name: string,
+    };
+
+    const [chatRows] = await dbPool.execute<ChatMessage[]>(
+      `SELECT
+        chat.message_id,
+        chat.hangout_member_id,
+        chat.message_content,
+        chat.message_timestamp,
+        hangout_members.display_name as sender_name
+      FROM
+        chat
+      LEFT JOIN
+        hangout_members ON chat.hangout_member_id = hangout_members.hangout_member_id
+      WHERE
+        chat.hangout_id = ?
+      ORDER BY
+        chat.message_timestamp DESC
+      LIMIT 20 OFFSET ?;`,
+      [requestData.hangoutID, requestData.messageOffset]
+    );
+
+    res.json({ success: true, chatMessages: chatRows })
+
+  } catch (err: unknown) {
+    console.log(err);
+    res.status(500).json({ success: false, message: 'Internal server error.' });
+  };
+});
