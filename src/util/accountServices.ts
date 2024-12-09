9adad00986a540ef8fad8076135bc384ccaa63b0
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2024-12-09 03:56:39 +0100

    Added accountServices.ts

---
+++ src/util/accountServices.ts
@@ -0,0 +1,35 @@
+import { Response } from "express";
+import { dbPool } from "../db/db";
+import { purgeAuthSessions } from "../auth/authSessions";
+import { removeRequestCookie } from "./cookieUtils";
+
+export async function handleIncorrectAccountPassword(res: Response, accountId: number, failedSignInAttempts: number): Promise<void> {
+  try {
+    await dbPool.execute(
+      `UPDATE
+        accounts
+      SET
+        failed_sign_in_attempts = failed_sign_in_attempts + 1
+      WHERE
+        account_id = ?;`,
+      [accountId]
+    );
+
+    const isLocked: boolean = failedSignInAttempts + 1 >= 5;
+
+    if (isLocked) {
+      await purgeAuthSessions(accountId, 'account');
+      removeRequestCookie(res, 'authSessionId', true);
+    };
+
+    res.status(401).json({
+      success: false,
+      message: `Incorrect password.${isLocked ? ' Account has been locked.' : ''}`,
+      reason: isLocked ? 'accountLocked' : undefined,
+    });
+
+  } catch (err: unknown) {
+    console.log(err);
+    res.status(500).json({ success: false, message: 'Internal server error.' });
+  };
+};
