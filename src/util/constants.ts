a760a3b823f004c7edde902930d0474ef2f87289
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-01-09 15:41:01 +0100

    Added missing number types to align with typing style.

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -2,28 +2,28 @@ export const minuteMilliseconds: number = 1000 * 60;
 export const hourMilliseconds: number = minuteMilliseconds * 60;
 export const dayMilliseconds: number = hourMilliseconds * 24;
 
-export const FAILED_SIGN_IN_LIMIT = 5;
-export const FAILED_ACCOUNT_UPDATE_LIMIT = 3;
-export const EMAILS_SENT_LIMIT = 3;
+export const FAILED_SIGN_IN_LIMIT: number = 5;
+export const FAILED_ACCOUNT_UPDATE_LIMIT: number = 3;
+export const EMAILS_SENT_LIMIT: number = 3;
 
-export const ACCOUNT_VERIFICATION_WINDOW = minuteMilliseconds * 20;
-export const ACCOUNT_RECOVERY_WINDOW = hourMilliseconds;
-export const ACCOUNT_DELETION_WINDOW = hourMilliseconds;
-export const ACCOUNT_DELETION_SUSPENSION_WINDOW = dayMilliseconds;
-export const ACCOUNT_EMAIL_UPDATE_WINDOW = dayMilliseconds;
-export const MAX_ONGOING_HANGOUTS_LIMIT = 12;
+export const ACCOUNT_VERIFICATION_WINDOW: number = minuteMilliseconds * 20;
+export const ACCOUNT_RECOVERY_WINDOW: number = hourMilliseconds;
+export const ACCOUNT_DELETION_WINDOW: number = hourMilliseconds;
+export const ACCOUNT_DELETION_SUSPENSION_WINDOW: number = dayMilliseconds;
+export const ACCOUNT_EMAIL_UPDATE_WINDOW: number = dayMilliseconds;
+export const MAX_ONGOING_HANGOUTS_LIMIT: number = 12;
 
-export const MAX_HANGOUT_MEMBERS_LIMIT = 20;
-export const MIN_HANGOUT_MEMBERS_LIMIT = 2;
+export const MAX_HANGOUT_MEMBERS_LIMIT: number = 20;
+export const MIN_HANGOUT_MEMBERS_LIMIT: number = 2;
 
-export const MIN_HANGOUT_PERIOD_DAYS = 1;
-export const MAX_HANGOUT_PERIOD_DAYS = 7;
+export const MIN_HANGOUT_PERIOD_DAYS: number = 1;
+export const MAX_HANGOUT_PERIOD_DAYS: number = 7;
 
-export const HANGOUT_VOTES_LIMIT = 3;
-export const HANGOUT_SUGGESTIONS_LIMIT = 3;
-export const HANGOUT_AVAILABILITY_SLOTS_LIMIT = 10;
+export const HANGOUT_VOTES_LIMIT: number = 3;
+export const HANGOUT_SUGGESTIONS_LIMIT: number = 3;
+export const HANGOUT_AVAILABILITY_SLOTS_LIMIT: number = 10;
 
-export const HANGOUT_AVAILABILITY_STAGE = 1;
-export const HANGOUT_SUGGESTIONS_STAGE = 2;
-export const HANGOUT_VOTING_STAGE = 3;
-export const HANGOUT_CONCLUSION_STAGE = 4;
+export const HANGOUT_AVAILABILITY_STAGE: number = 1;
+export const HANGOUT_SUGGESTIONS_STAGE: number = 2;
+export const HANGOUT_VOTING_STAGE: number = 3;
+export const HANGOUT_CONCLUSION_STAGE: number = 4;
