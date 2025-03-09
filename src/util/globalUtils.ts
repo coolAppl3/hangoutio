85bb6ec7de5722aea60c189da11a77429be5aa03
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-03-09 23:23:47 +0100

    Tiny refactors.

--- src/util/globalUtils.ts
+++ src/util/globalUtils.ts
@@ -2,7 +2,7 @@ export function getDateAndTimeString(timestamp: number): string {
   const dateObj: Date = new Date(timestamp);
 
   const date: number = dateObj.getDate();
-  const ordinalSuffix: string = getOrdinalSuffix(date);
+  const ordinalSuffix: string = getDateOrdinalSuffix(date);
 
   return `${getMonthName(dateObj)} ${date}${ordinalSuffix}, ${getTime(dateObj)}`;
 };
@@ -15,13 +15,17 @@ function getTime(date: Date): string {
   return new Intl.DateTimeFormat('en-GB', { hour: '2-digit', minute: '2-digit' }).format(date);
 };
 
-function getOrdinalSuffix(date: number): string {
-  if (date % 10 === 1 && date % 100 !== 11) return 'st';
-  if (date % 10 === 2 && date % 100 !== 12) return 'nd';
-  if (date % 10 === 3 && date % 100 !== 13) return 'rd';
+function getDateOrdinalSuffix(date: number): string {
+  if (date % 100 >= 11 && date % 100 <= 13) {
+    return 'th';
+  };
+
+  if (date % 10 === 1) return 'st';
+  if (date % 10 === 2) return 'nd';
+  if (date % 10 === 3) return 'rd';
 
   return 'th';
-}
+};
 
 export function containsInvalidWhitespace(string: string): boolean {
   if (string.trim() !== string) {
