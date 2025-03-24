0b986e76d803f537b78359019c39b95678c5322e
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-03-24 22:57:11 +0100

    Added hangout leadership transferring logic.

--- client/src/ts/modules/hangout/members/hangoutMembers.ts
+++ client/src/ts/modules/hangout/members/hangoutMembers.ts
@@ -1,6 +1,13 @@
+import axios, { AxiosError } from "../../../../../node_modules/axios/index";
+import { handleAuthSessionDestroyed, handleAuthSessionExpired } from "../../global/authUtils";
+import { ConfirmModal } from "../../global/ConfirmModal";
 import { createDivElement } from "../../global/domUtils";
+import LoadingModal from "../../global/LoadingModal";
 import popup from "../../global/popup";
+import { transferHangoutLeadershipService } from "../../services/hangoutMemberServices";
 import { globalHangoutState } from "../globalHangoutState";
+import { HangoutMember } from "../hangoutTypes";
+import { hangoutSuggestionState } from "../suggestions/hangoutSuggestions";
 import { createMemberElement } from "./membersUtils";
 
 interface HangoutMembersState {
@@ -19,6 +26,8 @@ export function hangoutMembers(): void {
 
 function loadEventListeners(): void {
   document.addEventListener('loadSection-members', renderMembersSection);
+
+  membersContainer?.addEventListener('click', handleMembersContainerClicks);
 };
 
 function renderMembersSection(): void {
@@ -45,4 +54,232 @@ function renderMembersContainer(): void {
 
   membersContainer.firstElementChild?.remove();
   membersContainer.appendChild(innerMembersContainer);
+};
+
+function handleMembersContainerClicks(e: MouseEvent): void {
+  if (!(e.target instanceof HTMLButtonElement)) {
+    return;
+  };
+
+  if (!globalHangoutState.data) {
+    return;
+  };
+
+  if (e.target.classList.contains('dropdown-menu-btn')) {
+    e.target.parentElement?.classList.toggle('expanded');
+    return;
+  };
+
+  const memberElement: HTMLDivElement | null = e.target.closest('.member');
+
+  if (!memberElement) {
+    return;
+  };
+
+  const memberIdString: string | null = memberElement.getAttribute('data-memberId');
+
+  if (!memberIdString || !Number.isInteger(+memberIdString)) {
+    return;
+  };
+
+  const hangoutMemberId: number = +memberIdString;
+  const hangoutMemberDisplayName: string | undefined = globalHangoutState.data.hangoutMembersMap.get(hangoutMemberId);
+
+  if (!hangoutMemberDisplayName) {
+    popup('Hangout member not found.', 'error');
+    renderMembersContainer();
+
+    return;
+  };
+
+  if (e.target.className === 'transfer-leadership-btn') {
+
+    confirmMemberAction(
+      `Are you sure you want to transfer the hangout leadership to ${hangoutMemberDisplayName}?`,
+      transferHangoutLeadership,
+      [hangoutMemberId]
+    );
+
+    return;
+  };
+
+  if (e.target.className === 'kick-member-btn') {
+    confirmMemberAction(
+      `Are you sure you want to kick ${hangoutMemberDisplayName} from the hangout?`,
+      kickHangoutMember,
+      [hangoutMemberId]
+    );
+
+    return;
+  };
+
+  if (e.target.className === 'waive-leadership-btn') {
+    confirmMemberAction(
+      `Are you sure you want to waive the hangout leadership?`,
+      waiveHangoutLeadership,
+      []
+    );
+
+    return;
+  };
+};
+
+async function transferHangoutLeadership(newLeaderMemberId: number): Promise<void> {
+  LoadingModal.display();
+
+  if (!globalHangoutState.data) {
+    popup('Something went wrong/', 'error');
+    LoadingModal.remove();
+
+    return;
+  };
+
+  const { hangoutId, hangoutMemberId, isLeader, hangoutDetails, hangoutMembersMap, hangoutMembers } = globalHangoutState.data;
+
+  if (!isLeader) {
+    popup(`You're not the hangout leader.`, 'error');
+    LoadingModal.remove();
+
+    return;
+  };
+
+  if (newLeaderMemberId === hangoutMemberId) {
+    popup(`You're already the hangout leader.`, 'error');
+    LoadingModal.remove();
+
+    return;
+  };
+
+  if (!hangoutMembersMap.has(newLeaderMemberId)) {
+    removeHangoutMemberData(newLeaderMemberId);
+    renderMembersSection();
+
+    popup('Hangout member not found.', 'error');
+    LoadingModal.remove();
+
+    return;
+  };
+
+  if (hangoutDetails.is_concluded) {
+    popup('Hangout has already been concluded.', 'error');
+    LoadingModal.remove();
+
+    return;
+  };
+
+  try {
+    await transferHangoutLeadershipService({ hangoutId, hangoutMemberId, newLeaderMemberId });
+
+    const newHangoutLeader: HangoutMember | undefined = hangoutMembers.find((member: HangoutMember) => member.hangout_member_id === newLeaderMemberId);
+
+    newHangoutLeader && (newHangoutLeader.is_leader = true);
+    globalHangoutState.data.isLeader = false;
+
+    renderMembersSection();
+
+    popup('Hangout leadership transferred.', 'error');
+    LoadingModal.remove();
+
+  } catch (err: unknown) {
+    console.log(err);
+    LoadingModal.remove();
+
+    if (!axios.isAxiosError(err)) {
+      popup('Something went wrong.', 'error');
+      return;
+    };
+
+    const axiosError: AxiosError<AxiosErrorResponseData> = err;
+
+    if (!axiosError.status || !axiosError.response) {
+      popup('Something went wrong.', 'error');
+      return;
+    };
+
+    const status: number = axiosError.status;
+    const errMessage: string = axiosError.response.data.message;
+    const errReason: string | undefined = axiosError.response.data.reason;
+
+    if (status === 400) {
+      popup('Something went wrong.', 'error');
+      return;
+    };
+
+    popup(errMessage, 'error');
+
+    if (status === 404) {
+      if (errReason === 'memberNotFound') {
+        removeHangoutMemberData(newLeaderMemberId);
+        renderMembersSection();
+
+        return;
+      };
+
+      LoadingModal.display();
+      setTimeout(() => window.location.reload(), 1000);
+
+      return;
+    };
+
+    if (status === 401) {
+      if (errReason === 'notHangoutLeader') {
+        globalHangoutState.data.isLeader = false;
+        renderMembersSection();
+
+        return;
+      };
+
+      if (errReason === 'authSessionExpired') {
+        handleAuthSessionExpired();
+        return;
+      };
+
+      handleAuthSessionDestroyed();
+    };
+  };
+};
+
+async function kickHangoutMember(hangoutMemberId: number): Promise<void> {
+  // TODO: implement
+};
+
+async function waiveHangoutLeadership(): Promise<void> {
+  // TODO: implement
+};
+
+function confirmMemberAction<T extends (...args: any[]) => Promise<void>>(confirmationString: string, func: T, args: Parameters<T>): void {
+  const confirmModal: HTMLDivElement = ConfirmModal.display({
+    title: null,
+    description: confirmationString,
+    confirmBtnTitle: 'Confirm',
+    cancelBtnTitle: 'Cancel',
+    extraBtnTitle: null,
+    isDangerousAction: true,
+  });
+
+  confirmModal.addEventListener('click', async (e: MouseEvent) => {
+    if (!(e.target instanceof HTMLButtonElement)) {
+      return;
+    };
+
+    if (e.target.id === 'confirm-modal-confirm-btn') {
+      await func(...args);
+      ConfirmModal.remove();
+
+      return;
+    };
+
+    if (e.target.id === 'confirm-modal-cancel-btn') {
+      ConfirmModal.remove();
+    };
+  });
+};
+
+function removeHangoutMemberData(hangoutMemberId: number): void {
+  if (!globalHangoutState.data) {
+    return;
+  };
+
+  globalHangoutState.data.hangoutMembersMap.delete(hangoutMemberId);
+  globalHangoutState.data.hangoutMembers = globalHangoutState.data.hangoutMembers.filter((member: HangoutMember) => member.hangout_member_id !== hangoutMemberId);
 };
