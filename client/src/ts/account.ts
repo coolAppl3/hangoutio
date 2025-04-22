b93ec46290069f2836f0c766df54d5f67cae9f75
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-04-22 02:36:28 +0200

    Implemented the module infrastructure for accounts.html.

--- client/src/ts/account.ts
+++ client/src/ts/account.ts
@@ -2,7 +2,14 @@ import '../scss/account.scss';
 
 import topNavbar from './modules/global/topNavbar';
 import botNavbar from './modules/global/botNavbar';
+import { accountFriends } from './modules/account/accountFriends';
+import { accountDetails } from './modules/account/accountDetails';
+import { initAccount } from './modules/account/initAccount';
 
 // initializing imports
 topNavbar();
 botNavbar();
+
+await initAccount();
+accountDetails();
+accountFriends();
