59e736a0bd9dbd293b8afdd40ed8d98a95e3d4ad
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-05-10 19:40:31 +0200

    Fixed some password reveal buttons not having a title attribute, and revealPassword() not updating both the buttons' titles and aria-labels.

--- client/src/ts/modules/global/revealPassword.ts
+++ client/src/ts/modules/global/revealPassword.ts
@@ -4,9 +4,31 @@ export default function revealPassword(passwordRevealBtn: HTMLButtonElement): vo
   if (passwordRevealBtn?.classList.contains('revealed')) {
     passwordRevealBtn.classList.remove('revealed');
     inputElement?.setAttribute('type', 'password');
+
+    updateBtnAttributes(passwordRevealBtn, false);
     return;
   };
 
   passwordRevealBtn?.classList.add('revealed');
   inputElement?.setAttribute('type', 'text');
+  updateBtnAttributes(passwordRevealBtn, true);
+};
+
+function updateBtnAttributes(passwordRevalBtn: HTMLButtonElement, passwordRevealed: boolean): void {
+  const btnTitle: string | null = passwordRevalBtn.getAttribute('title');
+  const btnAriaLabel: string | null = passwordRevalBtn.getAttribute('aria-label');
+
+  if (!btnTitle || !btnAriaLabel) {
+    return;
+  };
+
+  if (passwordRevealed) {
+    passwordRevalBtn.setAttribute('title', btnTitle.replace('Reveal', 'Hide'));
+    passwordRevalBtn.setAttribute('aria-label', btnAriaLabel.replace('Reveal', 'Hide'));
+
+    return;
+  };
+
+  passwordRevalBtn.setAttribute('title', btnTitle.replace('Hide', 'Reveal'));
+  passwordRevalBtn.setAttribute('aria-label', btnAriaLabel.replace('Hide', 'Reveal'));
 };
