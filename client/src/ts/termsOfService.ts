10407e3362a631ae4574b22e9ce0442cd15326d9
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-05-18 01:09:41 +0200

    Added missing disableBackForwardCache().

--- client/src/ts/termsOfService.ts
+++ client/src/ts/termsOfService.ts
@@ -2,6 +2,9 @@ import '../scss/terms-of-service.scss';
 
 import topNavbar from './modules/global/topNavbar';
 import botNavbar from './modules/global/botNavbar';
+import { disableBackForwardCache } from './modules/global/disableBackForwardCache';
+
+disableBackForwardCache();
 
 topNavbar();
 botNavbar();
