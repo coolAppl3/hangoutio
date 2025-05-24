4a24deda9c4b75aa8345de569be4b0a90856785a
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-05-24 23:20:53 +0200

    Implemented logic to render more hangout invitations.

--- client/src/ts/modules/account/accountHangoutInvites.ts
+++ client/src/ts/modules/account/accountHangoutInvites.ts
@@ -1,6 +1,11 @@
+import { handleAuthSessionExpired } from "../global/authUtils";
 import { HANGOUT_INVITES_FETCH_BATCH_SIZE } from "../global/clientConstants";
 import { getFullDateSTring } from "../global/dateTimeUtils";
 import { createBtnElement, createDivElement, createParagraphElement, createSpanElement } from "../global/domUtils";
+import { AsyncErrorData, getAsyncErrorData } from "../global/errorUtils";
+import LoadingModal from "../global/LoadingModal";
+import popup from "../global/popup";
+import { loadMoreHangoutInvitesService } from "../services/accountServices";
 import { HangoutInvite } from "./accountTypes";
 import { accountState } from "./initAccount";
 
@@ -18,6 +23,7 @@ const hangoutInvitesState: HangoutInvitesState = {
   allInvitesFetched: false,
 };
 
+const hangoutInvitesElement: HTMLDivElement | null = document.querySelector('#hangout-invites');
 const hangoutInvitesContainer: HTMLDivElement | null = document.querySelector('#hangout-invites-container');
 const loadMoreHangoutInvitesBtn: HTMLButtonElement | null = document.querySelector('#load-more-hangout-invites-btn');
 
@@ -28,12 +34,17 @@ export function initHangoutInvites(): void {
 
   loadEventListeners();
 
+  if (accountState.data.hangoutInvites.length === 0) {
+    loadMoreHangoutInvitesBtn?.classList.add('hidden');
+    return;
+  };
+
   insertHangoutInvites(accountState.data.hangoutInvites);
   hangoutInvitesContainer?.firstElementChild?.remove();
 };
 
 function loadEventListeners(): void {
-
+  hangoutInvitesElement?.addEventListener('click', handleHangoutInvitesElementClicks);
 };
 
 function insertHangoutInvites(hangoutInvites: HangoutInvite[]): void {
@@ -51,6 +62,77 @@ function insertHangoutInvites(hangoutInvites: HangoutInvite[]): void {
 
   hangoutInvitesState.allInvitesFetched = true;
   loadMoreHangoutInvitesBtn?.classList.add('hidden');
+};
+
+async function handleHangoutInvitesElementClicks(e: MouseEvent): Promise<void> {
+  if (!(e.target instanceof HTMLButtonElement)) {
+    return;
+  };
+
+  if (e.target.id === 'load-more-hangout-invites-btn') {
+    await loadMoreHangoutInvites();
+    return;
+  };
+
+  // TODO: continue implementation
+};
+
+async function loadMoreHangoutInvites(): Promise<void> {
+  LoadingModal.display();
+
+  if (!accountState.data) {
+    popup('Something went wrong.', 'error');
+    LoadingModal.remove();
+
+    return;
+  };
+
+  if (hangoutInvitesState.allInvitesFetched) {
+    loadMoreHangoutInvitesBtn?.classList.add('hidden');
+
+    popup('All invites have already been loaded.', 'success');
+    LoadingModal.remove();
+
+    return;
+  };
+
+  try {
+    const hangoutInvites: HangoutInvite[] = (await loadMoreHangoutInvitesService(hangoutInvitesState.currentOffset)).data;
+
+    accountState.data.hangoutInvites.push(...hangoutInvites);
+    hangoutInvitesState.currentOffset += hangoutInvitesState.offsetIncrement;
+
+    insertHangoutInvites(hangoutInvites);
+    LoadingModal.remove();
+
+    if (hangoutInvites.length < hangoutInvitesState.offsetIncrement) {
+      popup('All invitations loaded.', 'success');
+    };
+
+  } catch (err: unknown) {
+    console.log(err);
+    LoadingModal.remove();
+
+    const asyncErrorData: AsyncErrorData | null = getAsyncErrorData(err);
+
+    if (!asyncErrorData) {
+      popup('Something went wrong.', 'error');
+      return;
+    };
+
+    const { status, errMessage } = asyncErrorData;
+
+    if (status === 400) {
+      popup('Something went wrong.', 'error');
+      return;
+    };
+
+    popup(errMessage, 'error');
+
+    if (status === 401) {
+      handleAuthSessionExpired();
+    };
+  };
 };
 
 // --- --- ---
