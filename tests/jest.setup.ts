6ac885727643576d96d33f352246e1cd06acfe94
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-06-01 21:04:52 +0200

    Improved testing set up.

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,2 +1,17 @@
+require('dotenv').config();
+import { Request, Response, NextFunction } from 'express';
+
 process.env.DATABASE_NAME = process.env.TEST_DATABASE_NAME;
 process.env.PORT = '6000';
+
+jest.mock('../src/middleware/rateLimiter', () => ({
+  rateLimiter: jest.fn((req: Request, res: Response, next: NextFunction) => next()),
+}));
+
+jest.mock('../src/logs/errorLogger', () => ({
+  logUnexpectedError: jest.fn((req: Request, res: Response, next: NextFunction) => next()),
+}));
+
+jest.mock('../src/util/email/initTransporter', () => ({
+  sendEmail: jest.fn(() => null),
+}));
