27c746d58c71108c1a192a807a3d19c86e5f23a6
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-07-17 22:34:45 +0200

    Removed extra line break.

--- client/src/ts/modules/global/authUtils.ts
+++ client/src/ts/modules/global/authUtils.ts
@@ -8,7 +8,6 @@ export function handleAuthSessionExpired(afterAuthRedirectHref: string = window.
   removeAuthDetails(afterAuthRedirectHref, title, description);
 };
 
-
 export function handleAuthSessionDestroyed(afterAuthRedirectHref: string = window.location.href): void {
   const title: string = 'Invalid sign in credentials detected.';
   const description: string = `You've been signed out as a result. Please sign back in to continue.`;
