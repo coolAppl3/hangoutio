20349e5d6a3b64d91f280aec8370c1b7f99e5138
coolAppl3 <102565919+coolAppl3@users.noreply.github.com>
2025-07-20 00:38:43 +0200

    Refactored to use global domUtils functions.

--- client/src/ts/modules/global/ConfirmModal.ts
+++ client/src/ts/modules/global/ConfirmModal.ts
@@ -1,3 +1,5 @@
+import { createBtnElement, createDivElement, createParagraphElement } from "./domUtils";
+
 interface ConfirmModalConfig {
   title: string | null,
   description: string | null,
@@ -33,21 +35,18 @@ export class ConfirmModal {
   };
 
   private static createConfirmModal(config: ConfirmModalConfig): HTMLDivElement {
-    const confirmModal: HTMLDivElement = document.createElement('div');
-    confirmModal.id = 'confirm-modal';
+    const confirmModal: HTMLDivElement = createDivElement(null, 'confirm-modal');
     confirmModal.setAttribute('tabindex', '0');
     config.description && (confirmModal.className = 'has-description');
 
-    const confirmModalContainer: HTMLDivElement = document.createElement('div');
-    confirmModalContainer.id = 'confirm-modal-container';
-
-    config.title && confirmModalContainer.appendChild(this.createModalTitle(config.title));
+    const confirmModalContainer: HTMLDivElement = createDivElement(null, 'confirm-modal-container');
+    config.title && confirmModalContainer.appendChild(createParagraphElement('confirm-modal-title', config.title));
 
     if (config.description) {
-      const descriptionContainer: HTMLDivElement = this.createDescriptionContainer();
+      const descriptionContainer: HTMLDivElement = createDivElement('description-container');
 
       for (const descriptionLine of config.description.split('\n')) {
-        descriptionContainer.appendChild(this.createModalDescription(descriptionLine));
+        descriptionContainer.appendChild(createParagraphElement('confirm-modal-description', descriptionLine));
       };
 
       confirmModalContainer.appendChild(descriptionContainer);
@@ -59,48 +58,20 @@ export class ConfirmModal {
     return confirmModal;
   };
 
-  private static createModalTitle(title: string): HTMLParagraphElement {
-    const paragraphElement: HTMLParagraphElement = document.createElement('p');
-    paragraphElement.className = 'confirm-modal-title';
-    paragraphElement.appendChild(document.createTextNode(title));
-
-    return paragraphElement;
-  };
-
-  private static createDescriptionContainer(): HTMLDivElement {
-    const descriptionContainer: HTMLDivElement = document.createElement('div');
-    descriptionContainer.className = 'description-container';
-
-    return descriptionContainer;
-  };
-
-  private static createModalDescription(description: string): HTMLParagraphElement {
-    const paragraphElement: HTMLParagraphElement = document.createElement('p');
-    paragraphElement.className = 'confirm-modal-description';
-    paragraphElement.appendChild(document.createTextNode(description));
-
-    return paragraphElement;
-  };
-
   private static createBtnContainer(config: ConfirmModalConfig): HTMLDivElement {
-    const btnContainer: HTMLDivElement = document.createElement('div');
-    btnContainer.className = 'btn-container';
+    const btnContainer: HTMLDivElement = createDivElement('btn-container');
 
-    btnContainer.appendChild(this.createBtnElement('confirm-btn', config.confirmBtnTitle, config.isDangerousAction));
-    btnContainer.appendChild(this.createBtnElement('cancel-btn', config.cancelBtnTitle));
-    config.extraBtnTitle && btnContainer.appendChild(this.createBtnElement('other-btn', config.extraBtnTitle));
+    const confirmBtn: HTMLButtonElement = createBtnElement(null, config.confirmBtnTitle, 'confirm-modal-confirm-btn');
+    const cancelBtn: HTMLButtonElement = createBtnElement(null, config.cancelBtnTitle, 'confirm-modal-cancel-btn');
+
+    btnContainer.appendChild(confirmBtn);
+    btnContainer.appendChild(cancelBtn);
+
+    if (config.extraBtnTitle) {
+      const extraBtn: HTMLButtonElement = createBtnElement(null, config.extraBtnTitle, 'other-btn');
+      btnContainer.appendChild(extraBtn);
+    };
 
     return btnContainer;
-  };
-
-  private static createBtnElement(btnIdEnding: string, title: string, isDangerousAction?: boolean): HTMLButtonElement {
-    const btnElement: HTMLButtonElement = document.createElement('button');
-    btnElement.id = `confirm-modal-${btnIdEnding}`;
-    btnElement.setAttribute('type', 'button');
-    btnElement.appendChild(document.createTextNode(title));
-
-    isDangerousAction && (btnElement.className = 'danger');
-
-    return btnElement;
   };
 };
